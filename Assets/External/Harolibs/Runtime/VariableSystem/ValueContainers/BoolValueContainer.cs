7ce10dc2317ecfdf1a5cd954244175facb08f1b0
HaroUkino <haroukino@hotmail.com>
2026-06-02 20:29:34 -0300

    UPDATED: Harolibs

--- Assets/External/Harolibs/Runtime/VariableSystem/ValueContainers/BoolValueContainer.cs
+++ Assets/External/Harolibs/Runtime/VariableSystem/ValueContainers/BoolValueContainer.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 namespace HaroLibs {
     [CreateAssetMenu( fileName = nameof( BoolValueContainer ), menuName = HaroLibsConstPaths.SO_VARIABLE + nameof( BoolValueContainer ) )]
-    public class BoolValueContainer : ValueContainerBase<bool> { }
+    public class BoolValueContainer : ValueContainerBase<bool> { 
+        
+        public void Toggle() => Value = !Value;
+
+    }
 }
